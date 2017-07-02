b0a234486c0d8f5f642cb43c60d78a45310c8d18
Hanney <hanneyisworking@gmail.com>
2017-07-02 16:54:50 +0900

    Make the up button behave like the back button

--- app/src/main/java/com/example/android/popularmovie/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmovie/MovieDetailActivity.java
@@ -41,7 +41,7 @@ public class MovieDetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                onBackPressed();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
